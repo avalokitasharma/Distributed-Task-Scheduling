b68a915b4bbcfcf49fcab6b4c8eac988f934f764
Avi <avalokitasharma@gmail.com>
2026-05-01 00:23:25 +0530

    add tenant repo

--- auth-service/service/auth_service.go
+++ auth-service/service/auth_service.go
@@ -1,12 +1,54 @@
 package service
 
-import "github.com/avalokitasharma/job-scheduler/auth-service/repository"
+import (
+	"github.com/avalokitasharma/job-scheduler/auth-service/repository"
+	"github.com/avalokitasharma/job-scheduler/common/auth"
+	"github.com/google/uuid"
+	"golang.org/x/crypto/bcrypt"
+)
 
 type AuthService struct {
-	repo   *repository.UserRepo
-	secret string
+	userRepo   *repository.UserRepo
+	tenantRepo *repository.TenantRepo
+	secret     string
 }
 
-func NewAuthService(r *repository.UserRepo, secret string) *AuthService {
-	return &AuthService{repo: r, secret: secret}
+func NewAuthService(userRepo *repository.UserRepo, tenantRepo *repository.TenantRepo, secret string) *AuthService {
+	return &AuthService{
+		userRepo:   userRepo,
+		tenantRepo: tenantRepo,
+		secret:     secret,
+	}
+}
+
+func (s *AuthService) RegisterTenant(email, password, tenantName string) (string, error) {
+	hash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return "", err
+	}
+
+	tenant := &repository.Tenant{
+		ID:   uuid.NewString(),
+		Name: tenantName,
+	}
+
+	err = s.tenantRepo.CreateTenant(tenant)
+	if err != nil {
+		return "", err
+	}
+
+	user := &repository.User{
+		ID:       uuid.NewString(),
+		Email:    email,
+		Password: string(hash),
+		TenantID: tenant.ID,
+		Role:     "admin", // first user = admin
+	}
+
+	err = s.userRepo.Create(user)
+	if err != nil {
+		return "", err
+	}
+
+	return auth.GenerateJWT(s.secret, user.ID, tenant.ID, user.Role)
 }
--- go.mod
+++ go.mod
@@ -19,6 +19,7 @@ require (
 	github.com/goccy/go-yaml v1.19.2 // indirect
 	github.com/golang-jwt/jwt/v5 v5.3.1
 	github.com/golang-migrate/migrate/v4 v4.19.1 // indirect
+	github.com/google/uuid v1.6.0
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/cpuid/v2 v2.3.0 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
